470fb9a9b38929e31fe32d113ba739fd87ee6d3b
RICHARD BARBOSA PINHEIRO <39630333X@alunos.etec.itaqua>
2019-03-14 20:22:31 -0300

    adição do segundo passo

--- NoBullying.java
+++ NoBullying.java
@@ -1,7 +1,7 @@
 public class NoBullying { 
   public static void main(String[] args){ 
 	System.out.println('Programa NoBullying');  
-	System.out.println("Passo 1: Respeitar todas as divercidades de ser humano");
-	
+	System.out.println("Passo 1: Respeitar todas as diversidades de ser humano");
+	System.out.println("Passo 2: Apoio psicológico nas escolas ");
 	}
 } 
