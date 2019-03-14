fd63dac1c40f00ce3fe716fcb1baa841f6c6643a
RICHARD BARBOSA PINHEIRO <39630333X@alunos.etec.itaqua>
2019-03-14 20:48:32 -0300

    adição do sétimo passo

--- NoBullying.java
+++ NoBullying.java
@@ -1,12 +1,12 @@
 public class NoBullying { 
   public static void main(String[] args){ 
 	System.out.println('Programa NoBullying');  
-	System.out.println("Passo 1: Respeitar todas as diversidades de ser humano");
-	System.out.println("Passo 2: Apoio psicológico nas escolas ");
-	System.out.println("Passo 3: Sempre se colocar no lugar do outro!");
-	System.out.println("Passo 4: Conversar com os pais e amigos mais próximos sobre os problemaas");
-	System.out.println("Passo 5: oferecer ajuda a quem precisa");
-	System.out.println("Passo 6: Prestar atenção no comportamento do amigo");
-	
+	System.out.println("Passo 1: Respeitar todas as diversidades de ser humano.");
+	System.out.println("Passo 2: Apoio psicológico nas escolas. ");
+	System.out.println("Passo 3: Sempre se colocar no lugar do outro.!");
+	System.out.println("Passo 4: Conversar com os pais e amigos mais próximos sobre os problemaas.");
+	System.out.println("Passo 5: oferecer ajuda a quem precisa.");
+	System.out.println("Passo 6: Prestar atenção no comportamento do amigo.");
+	System.out.println("Passo 7: não julgar ninguem pelo seus atos,apenas ouvir.");
 	}
 } 
