dcc2319c9ba69e328a5896840b14fdeb721b563c
RICHARD BARBOSA PINHEIRO <39630333X@alunos.etec.itaqua>
2019-03-14 20:24:21 -0300

    adição do terceiro passo

--- NoBullying.java
+++ NoBullying.java
@@ -3,5 +3,7 @@ public class NoBullying {
 	System.out.println('Programa NoBullying');  
 	System.out.println("Passo 1: Respeitar todas as diversidades de ser humano");
 	System.out.println("Passo 2: Apoio psicológico nas escolas ");
+	System.out.println("Passo 3: Sempre se colocar no lugar do outro!");
+	
 	}
 } 
