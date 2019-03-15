ce951bdf1000dfea1038256a7f7f1bfff5478fa7
RICHARD BARBOSA PINHEIRO <39630333X@alunos.etec.itaqua>
2019-03-14 21:44:07 -0300

    commit final

--- NoBullying.java
+++ NoBullying.java
@@ -1,6 +1,6 @@
 public class NoBullying { 
   public static void main(String[] args){ 
-	System.out.println('Programa NoBullying');  
+	System.out.println("Programa NoBullying");  
 	System.out.println("Passo 1: Respeitar todas as diversidades de ser humano.");
 	System.out.println("Passo 2: Apoio psicológico nas escolas. ");
 	System.out.println("Passo 3: Sempre se colocar no lugar do outro.!");
